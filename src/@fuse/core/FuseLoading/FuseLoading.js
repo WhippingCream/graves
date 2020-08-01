72d85fe8411b0ac02ecb713ddcc7c2f8f561e16f
ZeroBoom <rsh921015@naver.com>
2020-08-02 01:54:36 +0900

    [FuseLoading 개선] 1. isShowingText prop 추가 2. isLeanerProgress prop 추가 (Leaner or Circular)

--- src/@fuse/core/FuseLoading/FuseLoading.js
+++ src/@fuse/core/FuseLoading/FuseLoading.js
@@ -1,5 +1,5 @@
 import { useTimeout } from '@fuse/hooks';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import { LinearProgress, CircularProgress } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
@@ -17,20 +17,30 @@ function FuseLoading(props) {
 
 	return (
 		<div className="flex flex-1 flex-col items-center justify-center">
-			<Typography className="text-20 mb-16" color="textSecondary">
-				Loading...
-			</Typography>
-			<LinearProgress className="w-xs" color="secondary" />
+			{props.isShowingText ? (
+				<Typography className="text-20 mb-16" color="textSecondary">
+					Loading...
+				</Typography>
+			) : null}
+			{props.isLinearProgress ? (
+				<LinearProgress className="w-xs" color="primary" />
+			) : (
+				<CircularProgress className="w-xs" color="primary" />
+			)}
 		</div>
 	);
 }
 
 FuseLoading.propTypes = {
-	delay: PropTypes.oneOfType([PropTypes.number, PropTypes.bool])
+	delay: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
+	isShowingText: PropTypes.bool,
+	isLinearProgress: PropTypes.bool
 };
 
 FuseLoading.defaultProps = {
-	delay: false
+	delay: false,
+	isShowingText: true,
+	isLinearProgress: true
 };
 
 export default FuseLoading;
