04393264f4440731d135aa3d1c64469bdb3175bb
ZeroBoom <rsh921015@naver.com>
2020-08-04 10:55:14 +0900

    카밀에서 에러로 오면 일단 dialog로 url, status, message 띄움

--- src/app/utility/camilleAxios.js
+++ src/app/utility/camilleAxios.js
@@ -1,9 +1,46 @@
 import axios from 'axios';
 import camilleRiotAuthService from 'app/services/camilleRiotAuthService';
+import store from 'app/store';
+import React from 'react';
+import * as fuseActions from 'app/store/actions';
+import Button from '@material-ui/core/Button';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const createCamilleAxios = () => {
 	const instance = axios.create();
+
 	instance.defaults.headers.common.RiotTokenId = camilleRiotAuthService.getAccessToken();
+	instance.interceptors.response.use(
+		response => {
+			return response;
+		},
+		error => {
+			store.dispatch(
+				fuseActions.openDialog({
+					children: (
+						<>
+							<DialogTitle id="alert-dialog-title">에러 발생!</DialogTitle>
+							<DialogContent>
+								<DialogContentText id="alert-dialog-description">
+									{error.config.url} <br />
+									{error.response.status}: {error.response.data.result}
+								</DialogContentText>
+							</DialogContent>
+							<DialogActions>
+								<Button onClick={() => store.dispatch(fuseActions.closeDialog())} color="primary" autoFocus>
+									확인
+								</Button>
+							</DialogActions>
+						</>
+					)
+				})
+			);
+			return Promise.reject(error);
+		}
+	);
 	return instance;
 };
 
